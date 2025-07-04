63bc5b8bf6bdd328bfbc45f25bfc796b543d97ee
Alph0xDev <alph0xdev@gmail.com>
2025-07-04 11:35:58 +0200

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,21 @@
+fx_version 'cerulean'
+
+game 'gta5'
+
+author 'RX STUDIOS'
+description 'New powerful notiftication system Standalone'
+version '1.0'
+
+lua54 'yes'
+
+ui_page 'html/ui.html'
+
+client_scripts {
+	'client.lua',
+}
+
+files {
+	'html/*.*'
+}
+
+export 'Alert'
